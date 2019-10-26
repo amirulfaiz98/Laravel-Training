5288c3d6dd581ca29030d984c45c59848200b49d
Amirul Faiz <amirulfaiz.work@gmail.com>
2019-10-26 22:29:58 +0800

    Create Post model with views

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+Auth::routes(['register'=>false]);
+
+Route::resource('post', 'PostController');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
