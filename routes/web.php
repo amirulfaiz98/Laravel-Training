75f610c031d2c6351ad0f6cbe5fdd53a77e929e9
Amirul Faiz <amirulfaiz.work@gmail.com>
2019-10-26 15:07:21 +0800

    Call Theme Globally

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/test', 'ThemeController@index')->name('test');
+Route::get('/bootstrap/test', 'ThemeController@index')->name('test');
 
 // Route::view('/test','index');
 
